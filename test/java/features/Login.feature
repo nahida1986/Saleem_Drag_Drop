11a4ce24a843aec19cccd25198ba067a3b871073
nahida1986 <55117820+nahida1986@users.noreply.github.com>
2019-10-07 14:48:17 -0400

    Add files via upload

---
+++ test/java/features/Login.feature
@@ -0,0 +1,12 @@
+Feature: Teat login functionality
+
+Scenario: Login with Wright Credintiol 
+
+
+Given User at landing page
+When User enter "komal" and "456"
+And Click on Login Button
+Then User should be on Homepage
+
+
+
---
+++ test/java/stepDefinitions/LoginstepDefinations.java
@@ -0,0 +1,35 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginstepDefinations {
+	
+	
+	
+	
+	   
+	 @Given("^User at landing page$")
+	    public void userAtLandingPage() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @When("^User enter \"([^\"]*)\" and \"([^\"]*)\"$")
+	    public void userEnterSomethingAndSomething(String strArg1, String strArg2) throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @Then("^User should be on Homepage$")
+	    public void userShouldBeOnHomepage() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	    @And("^Click on Login Button$")
+	    public void clickOnLoginButton() throws Throwable {
+	        throw new PendingException();
+	    }
+
+	}
